d5be814b0760d7cf601f7eb5008bfd1ee3a105c3
abdeali <56578305+abdealijaroli@users.noreply.github.com>
2025-05-25 08:20:27 +0530

    Time: 21 ms (37.12%) | Memory: 17.9 MB (52.96%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,30 +1,35 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        num_set = set()
+
+        l = 0
         max_len = 0
-        for i in range(len(s)):
-            curr_len = 0
-            seen = set()
-            for j in range(i, len(s)):
-                if s[j] not in seen:
-                    seen.add(s[j])
-                    curr_len += 1
-                    max_len = max(max_len, curr_len)
-                else: 
-                    break
-                
+        for r in range(len(s)):
+            while s[r] in num_set:
+                num_set.remove(s[l])
+                l += 1
+
+            max_len = max(max_len, r - l + 1)
+            num_set.add(s[r])
+
         return max_len
+
+
+
+        # max_len = 0
+        # for i in range(len(s)):
+        #     curr_len = 0
+        #     seen = set()
+        #     for j in range(i, len(s)):
+        #         if s[j] not in seen:
+        #             seen.add(s[j])
+        #             curr_len += 1
+        #             max_len = max(max_len, curr_len)
+        #         else: 
+        #             break
+                
+        # return max_len
                 
 
-
-        # num_set = set()
-
-        # l = 0
-
-        # for r in range(len(s)):
-        #     if s[r] in num_set:
-        #         num_set.remove(s[l])
-
-
-        #     num_set.add(s[r])
 
 
